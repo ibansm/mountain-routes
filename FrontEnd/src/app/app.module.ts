c0942e9357867e2df0de7cb585874f608cf22bdb
IakoGM <jakgonza@student.42urduliz.com>
2023-11-14 23:58:34 +0100

    working on navbar - resetCSS & some assets added

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { TipoRutaComponent } from './components/tipo-ruta/tipo-ruta.component';
 		ContactoComponent,
 		FavoritasComponent,
 		CreaRutaComponent,
-  TipoRutaComponent,
+  		TipoRutaComponent,
 	],
 	imports: [
 		BrowserModule,
