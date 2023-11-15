1617aae940f832e7b92ebb5b1515bdffea07a562
IakoGM <jakgonza@student.42urduliz.com>
2023-11-15 22:49:54 +0100

    navbar almost done

--- FrontEnd/src/app/components/menu/menu.component.ts
+++ FrontEnd/src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+	
+	toggle() : void {
+		document.querySelector('#navbarNav')?.classList.toggle('collapse')
+	}
 }
