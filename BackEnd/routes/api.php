1cd5af796fa0c639073cc8c16ddea021a968f631
birtjun <104511389+birtjun@users.noreply.github.com>
2023-11-16 16:48:26 +0100

    Update api.php

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -24,21 +24,37 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// API USUARIOS
-Route::apiResource('usuarios', UsuarioController::class);
-Route::get('usuarios/{usuario}/rutas', [UsuarioController::class, 'rutasPorUsuario']);
+//RUTAS PUBLICAS
 
-// API RUTAS
-Route::apiResource('rutas', RutaController::class);
-Route::get('rutas/ultimas/{ruta}', [RutaController::class, 'ultimasRutas']);
-Route::get('rutas/{ruta}/coordenadas', [RutaController::class, 'getCoordenadasRuta']);
-Route::get('rutas/busqueda/{filtro}', [RutaController::class, 'filtrarRuta']);
+    //API REGISTRO y LOGIN
+    Route::post('registrarse',[AuthController::class,'registrarse']);
+    Route::post('login',[AuthController::class,'login']);
+/////////////
 
-// API FOTOS_RUTA
-Route::apiResource('fotos-ruta', FotosRutaController::class, ['parameters' => ['fotos-ruta' => 'fotos']]);
+    // API USUARIOS
+    Route::apiResource('usuarios', UsuarioController::class);
+    Route::get('usuarios/{usuario}/rutas', [UsuarioController::class, 'rutasPorUsuario']);
+    
+    // API RUTAS
+    Route::apiResource('rutas', RutaController::class);
+    Route::get('rutas/ultimas/{ruta}', [RutaController::class, 'ultimasRutas']);
+    Route::get('rutas/{ruta}/coordenadas', [RutaController::class, 'getCoordenadasRuta']);
+    Route::get('rutas/busqueda/{filtro}', [RutaController::class, 'filtrarRuta']);
+    
+    // API FOTOS_RUTA
+    Route::apiResource('fotos-ruta', FotosRutaController::class, ['parameters' => ['fotos-ruta' => 'fotos']]);
+    
+    // API HISTORICO
+    Route::apiResource('historico', HistoricoController::class);
+    
+    // API INCIDENCIAS
+    Route::apiResource('incidencias', IncidenciaController::class);
 
-// API HISTORICO
-Route::apiResource('historico', HistoricoController::class);
+//RUTAS PRIVADAS
 
-// API INCIDENCIAS
-Route::apiResource('incidencias', IncidenciaController::class);
+Route::group(['middleware' => 'auth:sanctum'],function(){
+    
+    //Logout
+    Route::post('logout',[AuthController::class,'logout']);
+    
+});
