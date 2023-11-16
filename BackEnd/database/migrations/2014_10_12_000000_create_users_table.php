f0d92fe52d45376fc74cb0a7fc6c37f9176179e5
birtjun <104511389+birtjun@users.noreply.github.com>
2023-11-16 16:43:01 +0100

    Update 2014_10_12_000000_create_users_table.php

--- BackEnd/database/migrations/2014_10_12_000000_create_users_table.php
+++ BackEnd/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('nombre')->nullable();
+            $table->string('apellidos')->nullable();
+            $table->integer('edad')->nullable(); 
+            $table->enum('rol',['admin','user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
