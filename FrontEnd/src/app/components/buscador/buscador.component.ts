b1aaf7c21343a69f83808508dee7750ec4815b16
IakoGM <jakgonzalez@birt.eus>
2023-12-02 20:50:05 +0100

    wip buscador v.2

--- FrontEnd/src/app/components/buscador/buscador.component.ts
+++ FrontEnd/src/app/components/buscador/buscador.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { PeticionesService } from 'src/app/service/peticiones.service';
 
 @Component({
 	selector: 'app-buscador',
 	templateUrl: './buscador.component.html',
-	styleUrls: ['./buscador.component.scss']
+	styleUrls: ['./buscador.component.scss'],
+	providers: [PeticionesService]
 })
 export class BuscadorComponent {
+
+	buscadorData = {}
+
+	constructor(
+		private _peticiones: PeticionesService
+	) { }
+
+	public getCiudades() {
+		this._peticiones.getCiudades().subscribe({
+			next: data => {
+				console.log('La data de getCiudades(): ', data)
+			},
+			error: error => { }
+		})
+	}
+
+	buscadorForm() {
+		this._peticiones.buscadorForm(this.buscadorData).subscribe({
+			next: data => { },
+			error: error => { }
+		})
+	}
+
+	ngOnInit() {
+		this.getCiudades();
+	}
 
 	public userArray: any = [
 		"Iako",
@@ -29,5 +57,10 @@ export class BuscadorComponent {
 		"<2h",
 		">2h y <4h",
 		">4h y <6h"
+	]
+
+	public longitud: any = [
+		"< 5km",
+		"< 10km"
 	]
 }
--- FrontEnd/src/app/service/peticiones.service.ts
+++ FrontEnd/src/app/service/peticiones.service.ts
@@ -2,12 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PeticionesService {
-	constructor( public _http : HttpClient ) { }
-	
-	/* Aqui van todas las funciones que necesitemos para realizar las peticiones HTTP */
-	prueba() : Observable<any> {
-		return this._http.get('localhost:8000/users');
+
+	constructor(public _http: HttpClient) { }
+
+	/**
+	 * This function gets all the 'cities' from 'rutas'
+	 * @returns An observable to that endpoint
+	 */
+	getCiudades(): Observable<any> {
+		return this._http.get('http://localhost:8000/api/ciudades')
 	}
+	/**
+	 * This function gets all the 'dificultad' from 'rutas'
+	 * @returns An observable to that endpoint
+	 */
+	getDificultad(): Observable<any> {
+		return this._http.get('http://localhost:8000/api/dificultad')
+	}
+
+	/**
+	 * This function sends the data to be searched from 'rutas'
+	 * @param data -> The data to be searched
+	 * @returns An observable to that endpoint
+	 */
+	buscadorForm(data : any): Observable<any> {
+		return this._http.post('http://localhost:8000/api/buscador', data)
+	}
+
 }
