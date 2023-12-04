6e6e7b73a72067db90ed6c64422efe01703aa32b
Ivan Sola Marti <isola@birt.eus>
2023-12-04 15:22:12 +0100

    Dockerfile - Laravel & MySQL

---
+++ BackEnd/Docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+
+role=${CONTAINER_ROLE:-app}
+
+if [ "$role" = "app" ]; then
+    php artisan migrate:fresh --seed
+    php artisan import:script_carreras
+    php artisan key:generate
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan serve --port=$APP_PORT --host=0.0.0.0
+    exec docker-php-entrypoint "$@"
+fi
