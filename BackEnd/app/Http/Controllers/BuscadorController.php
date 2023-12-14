056a3d81268324a3ec072790291e79861bdc64b3
birtjun <104511389+birtjun@users.noreply.github.com>
2023-12-14 14:53:34 +0100

    Update BuscadorController.php

--- BackEnd/app/Http/Controllers/BuscadorController.php
+++ BackEnd/app/Http/Controllers/BuscadorController.php
@@ -27,4 +27,84 @@ class BuscadorController extends Controller
         return ApiResponse::success($dificultad,200);
 
     }
+
+    /*
+    public function buscar(Request $request){
+
+       $ciudad = $request->ciudad;
+       $dificultad = $request->dificultad;
+       $longitud = $request->longitud;
+       $duracion = $request->duracion;
+       $ninos = $request->ninos;
+
+       $rutas = Ruta::select('nombre','descripcion','ciudad','dificultad','longitud','duracion','ninos')
+                ->where('ninos',$ninos) 
+                ->when($ciudad,function($query,$ciudad){
+
+                    $query ->where('ciudad',$ciudad); 
+                
+                })
+
+                ->when($dificultad,function($query,$dificultad){
+
+                    $query ->where('dificultad',$dificultad); 
+                
+                })
+
+                ->when($longitud,function($query,$longitud){
+
+                    switch($longitud){
+
+                        case '5':
+                            return $query ->whereBetween('longitud',[0,$longitud]);
+                        break;
+
+                        case '10':
+                            return $query ->whereBetween('longitud',[5,$longitud]);
+                        break;
+
+                        //Falta el case mayor que 10
+                    } 
+                
+                })
+
+                ->when($duracion,function($query,$duracion){
+                    
+                    switch($duracion){
+
+                        case '2':
+                           
+                            return $query ->whereBetween('duracion',[0,120]);
+                        break;
+
+                        case '4':
+                            return $query ->whereBetween('duracion',[120,240]);
+                        break;
+
+                        case '6':
+                            return $query ->whereBetween('duracion',[240,360]);
+                        break;
+                    }
+                    
+                    
+                
+                })
+
+                
+        
+        ->get();
+
+        if ($rutas->isEmpty($rutas)) {
+
+            return ApiResponse::success('Lo sentimos,no hemos encontrado ninguna ruta',200);
+
+        }else{
+
+            return ApiResponse::success($rutas,200);
+        }
+       
+    }
+    
+*/
+    
 }
