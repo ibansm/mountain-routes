5410cbec9c29f661015323a1595b6de13ef6f4ce
IakoGM <jakgonza@student.42urduliz.com>
2024-01-03 15:55:32 +0100

    markers & polylines on map

--- FrontEnd/src/app/components/crea-ruta/crea-ruta.component.ts
+++ FrontEnd/src/app/components/crea-ruta/crea-ruta.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { LatLng, Map, polyline, tileLayer } from 'leaflet';
-import { Ruta, Dificultad } from 'src/app/models/ruta';
+import { LatLng, Map, Marker, Polyline, marker, polyline, tileLayer } from 'leaflet';
+import { Ruta } from 'src/app/models/ruta';
 import { PeticionesService } from 'src/app/service/peticiones.service';
 
 @Component({
@@ -20,6 +20,14 @@ export class CreaRutaComponent implements AfterViewInit {
 	// public duracion?: Number;
 	// public ninos?: Boolean;
 	public coordenadas: Array<LatLng> = [];
+	private last_coord: Polyline<any>[] = [];
+
+	// Triggers
+	private markerCounter = 0;
+	private polylineCounter = 1;
+
+	// Marcadores
+	private nuevoMarcador: Marker<any>[] = [];
 
 	constructor(
 		private _peticiones: PeticionesService
@@ -46,11 +54,44 @@ export class CreaRutaComponent implements AfterViewInit {
 			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(map);
 
-		map.on('click', (event) => {
-			this.nuevaRuta.coordenadas?.push(event.latlng);
-			polyline(this.coordenadas).addTo(map);
-			console.log(this.coordenadas);
-		})
+		
+		if (this.markerCounter === 0 && this.polylineCounter === 1) {
+			
+			/*
+				TODO => Comprobar que se puede guardar el RUTA
+			*/
+			map.on('click', (event) => {
+				this.coordenadas?.push(event.latlng);
+				this.last_coord.push(polyline(this.coordenadas).addTo(map));
+				console.log('PUSH - Coordenadas\n');
+				console.log(this.last_coord);
+			})
+
+			map.on('contextmenu', () => {
+				this.last_coord[this.last_coord.length - 1].remove()
+				this.last_coord.pop()
+				this.coordenadas.pop()
+				console.log('POP - Coordenadas\n');
+				console.log(this.last_coord);
+			})
+
+		} else if (this.markerCounter === 1 && this.polylineCounter === 0) {
+			
+			map.on('click', (e) => {
+				this.nuevoMarcador.push(marker(e.latlng, {draggable: true}).addTo(map));
+				console.log('PUSH\n');
+				console.log(this.nuevoMarcador);
+			})
+
+			map.on('contextmenu', (e) => {
+				this.nuevoMarcador[this.nuevoMarcador.length - 1].remove()
+				this.nuevoMarcador.pop()
+				console.log('POP\n');
+				console.log(this.nuevoMarcador);
+			})
+
+		}
+			
 
 	}
 
