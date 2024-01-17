827f1cf57f5545d5a4c59b8d689191f6a23dc031
IakoGM <jakgonza@student.42urduliz.com>
2024-01-17 13:14:23 +0100

    user_id added LS when reg

--- FrontEnd/src/app/components/create-acc/create-acc.component.ts
+++ FrontEnd/src/app/components/create-acc/create-acc.component.ts
@@ -22,29 +22,28 @@ export class CreateAccComponent {
     // Aquí puedes acceder a los datos del formulario a través de this.usuario
     console.log('Datos del usuario:', this.usuario);
 
-	// Recuperamos el user_id
-	this._peticiones.getUserId(this.usuario.email).subscribe({
-		next: data => {
-			console.log('USER_ID\n')
-			console.log(data.data[0].id)
-			localStorage.setItem('user_id', JSON.stringify(data.data[0].id))
-		},
-		error: error => { console.log('Error a la hora de recuperar el user_id ' + error)}
-	})
-
     // También puedes acceder a los controles del formulario
     console.log(localStorage.getItem("token"));
     this._peticiones.postRegistrarse(this.usuario).subscribe({
-          next: data => {
-            localStorage.setItem("access_token", JSON.stringify(data.data.access_token));
-            localStorage.setItem("token_type", JSON.stringify(data.data.token_type));
-			this._peticiones.isLogged = true
-            this.router.navigate(['/home']);
-            console.log('Datos del usuario1:', data)
-          },
-          error: error => {
-            console.log('Error a la hora de guardar la ruta.\n' + error);
-          }
+		next: data => {
+		localStorage.setItem("access_token", JSON.stringify(data.data.access_token));
+		localStorage.setItem("token_type", JSON.stringify(data.data.token_type));
+		this._peticiones.isLogged = true
+		// Recuperamos el user_id
+		this._peticiones.getUserId(this.usuario.email).subscribe({
+			next: data => {
+				console.log('USER_ID\n')
+				console.log(data.data[0].id)
+				localStorage.setItem('user_id', JSON.stringify(data.data[0].id))
+			},
+			error: error => { console.log('Error a la hora de recuperar el user_id ' + error)}
+		})
+		console.log('Datos del usuario1:', data)
+		this.router.navigate(['/home']);
+		},
+		error: error => {
+		console.log('Error a la hora de guardar la ruta.\n' + error);
+		}
     })
   }
 }
